aea77573e8a97583936f5766b38b89f0d4d36786
unknown <josetf@INGSI08.AUDIFARMA.D1>
2014-10-27 18:45:07 -0500

    inicial

---
+++ cotejo/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\wamp\www\cotejo\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\wamp\www\cotejo\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ cotejo/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+c:\wamp\www\cotejo\platforms\android\ant-build\AndroidManifest.xml : \
+c:\wamp\www\cotejo\platforms\android\AndroidManifest.xml \
