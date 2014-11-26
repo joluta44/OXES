fed15c08d520eea31b31f1b6bee283439b2bbe42
juliano <juliano@INGSI12>
2014-11-26 17:32:07 -0500

    Ingreso de usuarios.

--- cotejo/www/static/javascripts/app.js
+++ cotejo/www/static/javascripts/app.js
@@ -1,18 +1,18 @@
-var App = (function(lng, undefined) {
+var App = (function (lng, undefined) {
 
-    sectionTrigger = function(event) {
+    sectionTrigger = function (event) {
         event.stopPropagation();
-        setTimeout(function() {
+        setTimeout(function () {
             lng.Notification.success("Event: " + event.type, "Layout events manager", "info", 2);
         }, 500);
     };
 
-    articleTrigger = function(event) {
+    articleTrigger = function (event) {
         event.stopPropagation();
         console.error(event);
     };
 
-    environment = function(event) {
+    environment = function (event) {
         var environment = lng.Core.environment();
         var el = lng.dom("section > article#environment");
 
@@ -36,76 +36,62 @@ var App = (function(lng, undefined) {
 App.carousel = {prev: null, next: null};
 
 Lungo.Events.init({
-    'load section#layoutevents'     : App.sectionTrigger,
-
-    'unload section#layoutevents'   : App.sectionTrigger,
-
-    'load article#environment'      : App.environment,
-
-    'load article#touchevents'      : function(event) {
+    'load section#layoutevents': App.sectionTrigger,
+    'unload section#layoutevents': App.sectionTrigger,
+    'load article#environment': App.environment,
+    'load article#touchevents': function (event) {
 
         ["singleTap", "doubleTap", "hold",
             "swipe", "-swiping", "swipeLeft", "swipeRight", "swipeUp", "swipeDown",
             "rotate", "rotateLeft", "rotateRight",
             "pinch", "pinchIn", "pinchOut",
-            "drag", "dragLeft", "dragRight", "dragUp", "dragDown"].forEach(function(type) {
-            $$("article#touchevents #gestures").on(type, function(event) {
+            "drag", "dragLeft", "dragRight", "dragUp", "dragDown"].forEach(function (type) {
+            $$("article#touchevents #gestures").on(type, function (event) {
                 $$(this).siblings('.console.output').append(' | ' + type);
             });
         });
 
-        $$("[data-action=clean_console]").tap(function(event) {
+        $$("[data-action=clean_console]").tap(function (event) {
             $$('.console.output').html("");
         });
 
-        $$("[data-action=twitter]").tap(function(event) {
+        $$("[data-action=twitter]").tap(function (event) {
             window.open("https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Flungo.tapquo.com%2F&text=@lungojs a framework for developers who want to design, build and share cross device apps", "_blank");
         });
 
     },
-
-
-    'load section#carousel': function(event) {
-        App.carousel = Lungo.Element.Carousel($$('[data-control=carousel]')[0], function(index, element) {
+    'load section#carousel': function (event) {
+        App.carousel = Lungo.Element.Carousel($$('[data-control=carousel]')[0], function (index, element) {
             Lungo.dom("section#carousel .title span").html(index + 1);
         });
     },
-
-    'tap section#carousel > header [data-direction=left]':  App.carousel.prev,
-
+    'tap section#carousel > header [data-direction=left]': App.carousel.prev,
     'tap section#carousel > header [data-direction=right]': App.carousel.next,
-
-    'load section#pull': function(event) {
+    'load section#pull': function (event) {
         App.pull = new Lungo.Element.Pull('section#pull article', {
             onPull: "Pull down to refresh",
             onRelease: "Release to get new data",
             onRefresh: "Refreshing...",
-            callback: function() {
+            callback: function () {
                 alert("Pull & Refresh completed!");
                 App.pull.hide();
             }
         });
     },
-
-
-    'touch article#notification a[data-action=normal]': function() {
+    'touch article#notification a[data-action=normal]': function () {
         Lungo.Notification.show('user', 'Title', 2);
     },
-
-    'touch article#notification a[data-action=loading]': function() {
+    'touch article#notification a[data-action=loading]': function () {
         Lungo.Notification.show();
         setTimeout(Lungo.Notification.hide, 3000);
     },
-
-    'touch article#notification a[data-action=success]': function() {
+    'touch article#notification a[data-action=success]': function () {
         Lungo.Notification.success('Title', 'Description', 'ok', 2);
     },
-
-    'touch article#notification a[data-action=error]': function() {
+    'touch article#notification a[data-action=error]': function () {
         Lungo.Notification.error('Title', 'Description', 'remove', 2);
     },
-
-    'touch article#notification a[data-action=confirm]': function() {
+    'touch article#notification a[data-action=confirm]': function () {
         Lungo.Notification.confirm({
             icon: 'user',
             title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
@@ -113,24 +99,26 @@ Lungo.Events.init({
             accept: {
                 icon: 'checkmark',
                 label: 'Accept',
-                callback: function(){ alert("Yes!"); }
+                callback: function () {
+                    alert("Yes!");
+                }
             },
             cancel: {
                 icon: 'close',
                 label: 'Cancel',
-                callback: function(){ alert("No!"); }
+                callback: function () {
+                    alert("No!");
+                }
             }
         });
     },
-
-    'touch article#notification a[data-action=html]': function() {
+    'touch article#notification a[data-action=html]': function () {
         Lungo.Notification.html('<h1>Hello World</h1>', "Close");
     },
-
-    'touch article#notification a[data-action=chaining]': function() {
-        Lungo.Notification.show('user', 'user', 2, function() {
-            Lungo.Notification.error('Title 2', 'Description 2', 'remove',  2, function() {
-                Lungo.Notification.show('cog', 'cog', 2, function() {
+    'touch article#notification a[data-action=chaining]': function () {
+        Lungo.Notification.show('user', 'user', 2, function () {
+            Lungo.Notification.error('Title 2', 'Description 2', 'remove', 2, function () {
+                Lungo.Notification.show('cog', 'cog', 2, function () {
                     Lungo.Notification.html('<h1>Hello World</h1>', "Close");
                 });
             });
@@ -139,7 +127,7 @@ Lungo.Events.init({
 
 });
 
-Lungo.ready(function() {
+Lungo.ready(function () {
 
     // Lungo.Aside.show();
     // Lungo.Router.section("notification");
@@ -169,5 +157,4 @@ Lungo.ready(function() {
     // });
     // Lungo.Notification.html("<h1 class='title'>Title</h1><article>aslkdkals</article><a href='#' class='button large anchor' >Seleccionar</a>", "Cancelar");
     // Lungo.Notification.push("Lorem ipsum dolor sit amet", "home");
-
 });
