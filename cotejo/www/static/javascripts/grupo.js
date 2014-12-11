24b6d28dc7b6cc6fab1bbc05a5a3f9f8b2da44d6
juliano <juliano@INGSI12>
2014-12-11 17:26:32 -0500

    Creación de Eventos

--- cotejo/www/static/javascripts/grupo.js
+++ cotejo/www/static/javascripts/grupo.js
@@ -1,3 +1,7 @@
 var grupo = {
     administrador: null, tipoEvento: null, descripcion: null, distintivo: null, nombre: null, tipoGrupo: null, alias: null, codigo: 0    
 };
+
+var grupoRival = {
+    administrador: null, tipoEvento: null, descripcion: null, distintivo: null, nombre: null, tipoGrupo: null, alias: null, codigo: 0    
+};
