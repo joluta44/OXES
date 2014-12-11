49e17615e324739c6031ab4001eac81ca10f4df7
juliano <juliano@INGSI12>
2014-12-11 17:26:41 -0500

    Creación de Eventos

--- cotejo/www/static/javascripts/evento.js
+++ cotejo/www/static/javascripts/evento.js
@@ -1,6 +1,7 @@
 document.write("<script type='text/javascript' src='static/javascripts/util.js'></script>");
 document.write("<script type='text/javascript' src='static/javascripts/usuario.js'></script>");
 document.write("<script type='text/javascript' src='static/javascripts/sitio.js'></script>");
+document.write("<script type='text/javascript' src='static/javascripts/grupo.js'></script>");
 
 var evento = {
     fecha: null, listaGrupos: null, sitio: null, tipoEvento: null, administrador: null, codigo: 0
@@ -17,8 +18,10 @@ function crearEvento() {
     usuario.apellido = sessionStorage.getItem("apellido");
 
     sitio.codigo = 123;
+    grupo.codigo = 1;
+    grupoRival.codigo = 2;
 
-    var data = JSON.stringify({evento: evento, usuario: usuario, sitio: sitio});
+    var data = JSON.stringify({evento: evento, usuario: usuario, sitio: sitio, grupo: grupo, grupoRival: grupoRival});
 
     $.ajax({
         url: url,
